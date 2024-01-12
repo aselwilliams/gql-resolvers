652b9a8821121ccf32e532ae81ec750a603f7755
Asel Williams <aselwilliams22@gmail.com>
2024-01-12 17:52:02 -0600

    initial commit

---
+++ server/src/index.js
@@ -0,0 +1,32 @@
+const { ApolloServer } = require('@apollo/server');
+const { startStandaloneServer } = require('@apollo/server/standalone');
+const resolvers = require("./resolvers");
+const TrackAPI = require("./datasources/track-api");
+
+const typeDefs = require('./schema');
+
+async function startApolloServer() {
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+  const { url } = await startStandaloneServer(server, {
+    //TODO: configure server options
+    context: async ()=> {
+      const {cache} = server;
+
+      return {
+        dataSources: {
+          trackAPI: new TrackAPI({cache}),
+        },
+      };
+    },
+  });
+
+  console.log(`
+      🚀  Server is running
+      📭  Query at ${url}
+    `);
+}
+
+startApolloServer();
